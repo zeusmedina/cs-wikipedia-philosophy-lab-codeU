b72b1cb0590d1b53723998d2c4bfb245f8a339ed
Allen Downey <downey@allendowney.com>
2016-03-24 11:25:22 -0400

    Removing solutions

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
 	
-	final static List<String> visited = new ArrayList<String>();
 	final static WikiFetcher wf = new WikiFetcher();
 	
 	/**
@@ -27,66 +29,16 @@ public class WikiPhilosophy {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-		String destination = "https://en.wikipedia.org/wiki/Philosophy";
-		String source = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		
-		testConjecture(destination, source, 10);		
-	}
-
-	/**
-	 * Starts from given URL and follows first link until it finds the destination or exceeds the limit.
-	 * 
-	 * @param destination
-	 * @param source
-	 * @throws IOException
-	 */
-	public static void testConjecture(String destination, String source, int limit) throws IOException {
-		String url = source;
-		for (int i=0; i<limit; i++) {
-			if (visited.contains(url)) {
-				System.err.println("We're in a loop, exiting.");
-				return;
-			} else {
-				visited.add(url);
-			}
-			Element elt = getFirstValidLink(url);
-			if (elt == null) {
-				System.err.println("Got to a page with no valid links.");
-				return;
-			}
-			
-			System.out.println("**" + elt.text() + "**");
-			url = elt.attr("abs:href");
-			
-			if (url.equals(destination)) {
-				System.out.println("Eureka!");
-				break;
-			}
-		}
-	}
-
-	/**
-	 * Loads and parses a URL, then extracts the first link.
-	 * 
-	 * @param url
-	 * @return the Element of the first link, or null.
-	 * @throws IOException
-	 */
-	public static Element getFirstValidLink(String url) throws IOException {
-		print("Fetching %s...", url);
+		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		Elements paragraphs = wf.fetchWikipedia(url);
-		WikiParser wp = new WikiParser(paragraphs);
-		Element elt = wp.findFirstLink();
-		return elt;
-	}
 
-	/**
-	 * Formats and print the arguments.
-	 * 
-	 * @param msg
-	 * @param args
-	 */
-	private static void print(String msg, Object... args) {
-		System.out.println(String.format(msg, args));
+		Element firstPara = paragraphs.get(0);
+		
+		Iterable<Node> iter = new WikiNodeIterable(firstPara);
+		for (Node node: iter) {
+			if (node instanceof TextNode) {
+				System.out.print(node);
+			}
+        }        
 	}
 }
