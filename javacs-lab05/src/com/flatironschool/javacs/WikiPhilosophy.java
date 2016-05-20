0498910d75302d030a362c59739f57ba22239b0d
zeusmedina <zeusmedina2@gmail.com>
2016-05-20 11:29:38 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -13,6 +13,7 @@ import org.jsoup.select.Elements;
 public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
+  final static List<String> visitedLinks = new ArrayList<String>();
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -30,22 +31,105 @@ public class WikiPhilosophy {
 	public static void main(String[] args) throws IOException {
 		
         // some example code to get you started
+    String startUrl = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+		String endUrl =  "https://en.wikipedia.org/wiki/Philosophy";
+	
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
+    testPath(startUrl, endUrl);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
+  }
+
+  public static void testPath(String startUrl, String endUrl) throws IOException {
+    for(int i = 0; i < 11; i++) {
+      if(visitedLinks.contains(startUrl)) {
+        System.out.println("Ended back at start page");
+          return;
+      } else {
+         visitedLinks.add(startUrl);
+      }
+      
+      Element elm = findFirstValidLink(startUrl);
+      if(elm == null) {
+        System.out.println("No links on this page");
+        return;
+      }
+      startUrl = elm.attr("abs:href");
+      System.out.println(startUrl);
+      if(startUrl.equals(endUrl)) {
+        System.out.println("The myth is true!");
+        break;
+      }
+  }
+
+  }
+
+  public static Element findFirstValidLink(String url) throws IOException {
+    System.out.println("Searching path...");
+    Elements paragraphs = wf.fetchWikipedia(url);
+    Element elt = findFirstLink(paragraphs);
+    return elt;
+  }
+
+  public static Element findFirstLink(Elements paragraphs) {
+    for(Element paragraph: paragraphs) {
+      Element firstLink = findFirstLinkInPara(paragraph);
+      if(firstLink != null) {
+        return firstLink;
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  public static Element findFirstLinkInPara(Node root) {
+    Iterable<Node> wikiNode = new WikiNodeIterable(root);
+
+    for(Node node: wikiNode) {
+      if(node instanceof Element) {
+        Element firstLink = checkElementForLink((Element) node);
+        if(firstLink != null) {
+          return firstLink;
+        }
+      }
+    }
+    return null;
+  }
+
+  public static Element checkElementForLink(Element elt) {
+    if(isValidLink(elt)) {
+      return elt;
+    }
+    return null;
+  }
+
+  public static boolean isValidLink(Element elm) {
+    if(!elm.tagName().equals("a")) {
+      return false;
+    }
+    if (isItalic(elm)) {
+      return false;
+    }
+    if(hasUpperCase(elm)) {
+      return false;
+    }
+    return true;
+  }
+
+  public static boolean isItalic(Element start) {
+    for (Element elt=start; elt != null; elt = elt.parent()) {
+      if (elt.tagName().equals("i") || elt.tagName().equals("em")) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static boolean hasUpperCase(Element elm) {
+    String wikiPageTitle = elm.text();
+    boolean hasUppercase = !wikiPageTitle.equals(wikiPageTitle.toLowerCase());
+    return hasUppercase;
+  }
+
 }
